4097ccd70b261c2bb727a380afb2534d49a2d5a0
unknown <victorkipronokirui2@gmail.com>
2024-07-28 22:33:54 +0300

    navbar background

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <nav
-      className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 
+      className="bg-white fixed w-full z-20 top-0 start-0 
       border-b border-gray-200 dark:border-gray-600"
     >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
