1999ea8767e17bdb7b7576b032f828f74a210c7c
unknown <victorkipronokirui2@gmail.com>
2024-07-28 22:29:03 +0300

    icons

--- src/components/homepage/Features.jsx
+++ src/components/homepage/Features.jsx
@@ -1,118 +1,65 @@
 import React from "react";
+import { FaListUl } from "react-icons/fa6";
+import { FaGlobe } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { MdCurrencyExchange } from "react-icons/md";
 
+const features = [
+  {
+    id: 1,
+    title: "Instant",
+    description:
+      "With access to the GSP Tier 2 platform, you have access into a whole new world where you can easily convert GSP funds to top cryptocurrencies.",
+    icon: <FaListUl className="text-green-500 text-2xl w-10 h-10" />,
+  },
+  {
+    id: 2,
+    title: "Global Acknoledgement ",
+    description:
+      "The GSP funds in Tier 2 platform are globally accepted assets all around different continents. ",
+    icon: <FaGlobe className="text-green-500 text-2xl w-10 h-10" />,
+  },
+  {
+    id: 3,
+    title: "Versatille",
+    description: "Availability in different continents and accessible to all.",
+    icon: <MdCurrencyExchange className="text-green-500 text-2xl w-10 h-10" />,
+  },
+  {
+    id: 4,
+    title: "Safe and Secure",
+    description:
+      "With the introduction of security phrase, our GSP funds are secured more than ever thereby making it highly valued worldwide.",
+    icon: <FaLock className="text-green-500 text-2xl w-10 h-10" />,
+  },
+];
 export default function Features() {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white">
       <div className="container px-4 md:px-12 py-12 mx-auto">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-4 md:grid-cols-2 border rounded-lg shadow p-4 md:p-6 text-center">
-          <div className="flex flex-wrap flex-col items-center justify-center px-1 md:px-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 dark:text-gray-200"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
-              />
-            </svg>
+          {features.map((feature, index) => {
+            return (
+              <div
+                className="flex flex-wrap flex-col items-center justify-center px-6"
+                key={index}
+              >
+                <div className="flex items-center justify-center w-20 h-20 bg-white border-4 border-green-500 rounded-full">
+                  <div className="flex items-center justify-center w-16 h-16 bg-white rounded-full">
+                    {feature.icon}
+                  </div>
+                </div>
 
-            <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-300">
-              Desktop Support
-            </h1>
+                <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-300">
+                  {feature.title}
+                </h1>
 
-            <p className="mt-2 text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim
-              fusce tortor, ac sed malesuada blandit.
-            </p>
-          </div>
-
-          <div className="flex flex-wrap flex-col items-center justify-center px-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 dark:text-gray-200"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-              />
-            </svg>
-
-            <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-300">
-              Fully Responsive
-            </h1>
-
-            <p className="mt-2 text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim
-              fusce tortor, ac sed malesuada blandit.
-            </p>
-          </div>
-
-          <div className="flex flex-wrap flex-col items-center justify-center px-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 dark:text-gray-200"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
-              />
-            </svg>
-
-            <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-300">
-              Fully Responsive
-            </h1>
-
-            <p className="mt-2 text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim
-              fusce tortor, ac sed malesuada blandit.
-            </p>
-          </div>
-
-          <div className="flex flex-wrap flex-col items-center justify-center px-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 dark:text-gray-200"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
-
-            <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-300">
-              Settings
-            </h1>
-
-            <p className="mt-2 text-gray-500 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim
-              fusce tortor, ac sed malesuada blandit.
-            </p>
-          </div>
+                <p className="mt-2 text-gray-500 dark:text-gray-400">
+                  {feature.description}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
